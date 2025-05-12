ea7c8663bd0513a4c0bb129ba85a63ac2fc08d92
ParkByungYoon <bonbak1101@gmail.com>
2025-05-12 12:55:38 +0900

    remove fullformer

--- model/Fullformer.py
+++
@@ -1,7 +0,0 @@
-from model.Crossformer import *
-from layer.Fullformer import FullAttentionTransformerEncoder
-
-class Fullformer(Crossformer):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.transformer_encoder = FullAttentionTransformerEncoder(self.output_dim, self.input_len, self.segment_len)
